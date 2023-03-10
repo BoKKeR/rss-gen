0bf085a8a4115f4ed76e03ffcaff11d7e9fe9716
Norbert Takacs <norbert.takacs@postnord.com>
2023-03-10 17:57:10 +0100

    v2

--- src/pages/api/rss.ts
+++ src/pages/api/rss.ts
@@ -31,7 +31,7 @@ const get = async (req: NextApiRequest, res: NextApiResponse) => {
     link: string;
     content: string;
   }[];
-  const root = builder.create("rss", { version: "1.0", encoding: "UTF-8" });
+  const root = builder.create("rss", { version: "2.0" });
   const channel = root.ele("channel");
   channel.ele("title", undefined, "RSS-GEN");
   channel.ele("description", undefined, "test rss feed");
